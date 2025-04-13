d6d054cdb1ad59da9446951a404cd2c2b9921467
Muhammad Hasnat Ahmad <hxncodes@gmail.com>
2025-04-13 22:23:15 +0500

    ios build & static assets copied

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -6,6 +6,9 @@ const _layout = () => {
   return (
     <Tabs>
         <Tabs.Screen name="index" options = {{title: 'Home',headerShown: false}} />
+        <Tabs.Screen name="profile" options = {{title: 'Profile', headerShown: false}}/>
+        <Tabs.Screen name="saved" options = {{title: 'Saved', headerShown: false}}/>
+        <Tabs.Screen name="search" options = {{title: 'Search', headerShown: false}}/>
     </Tabs>
   )
 }

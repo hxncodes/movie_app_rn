318b4fb4f10f4dd2cc6417f9d46d705473692110
Muhammad Hasnat Ahmad <hxncodes@gmail.com>
2025-04-13 14:21:01 +0500

    Basic Navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,10 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-bold text-blue-500">hxnCodes</Text>
+      <Text className="text-3xl font-bold text-dark-200">hxnCodes</Text>
     </View>
   );
 }

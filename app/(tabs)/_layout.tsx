82f7f2cf0d937cae5121d039f5eb018fcb6000a2
Muhammad Hasnat Ahmad <hxncodes@gmail.com>
2025-04-16 16:39:33 +0500

    Bottom Navbar with routing completed

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -9,7 +9,7 @@ const TabIcon = ({ focused, icon, title }: any) => {
     return (
       <ImageBackground
         source={images.highlight}
-        className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
+        className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden"
       >
         <Image source={icon} tintColor="#151312" className="size-5" />
         <Text className="text-secondary text-base font-semibold ml-2">
@@ -19,13 +19,26 @@ const TabIcon = ({ focused, icon, title }: any) => {
     );
   }
   return (
-    <View className="size-full justify-center items-center mt-4 rounded-full"><Image source={icon} tintColor='#A8B5DB' className="size-5"/></View>
-  )
+    <View className="size-full justify-center items-center mt-4 rounded-full">
+      <Image source={icon} tintColor="#A8B5DB" className="size-5" />
+    </View>
+  );
 };
 
 const _layout = () => {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarItemStyle: {
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        },
+        tabBarStyle: { backgroundColor: "#0f0D23", borderRadius: 50,marginHorizontal: 20, marginBottom: 36, height: 52, position: 'absolute', overflow:'hidden', borderWidth:1, borderColor:'#0f0D23' },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{

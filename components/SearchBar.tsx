998d42374d75d1cafdeadbd7cc09232ab4d13187
Muhammad Hasnat Ahmad <hxncodes@gmail.com>
2025-04-20 23:16:38 +0500

    SearchBar with expo-router

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,12 +1,18 @@
-import { View, Text, Image } from 'react-native'
+import { View, TextInput, Image } from 'react-native'
 import React from 'react'
-import { icons } from '@/constants/icons'
+import { icons } from '@/constants/icons';
 
-const SearchBar = () => {
+interface Props {
+    placeholder: string;
+    onPress?: () => void;
+}
+
+const SearchBar = ({placeholder, onPress}:Props) => {
   return (
     <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-4'>
-        <Image source={icons.search} className='size-5' />
-      <Text>SearchBar</Text>
+        <Image source={icons.search} className='size-5' resizeMode='contain' tintColor="#ab8bff"/>
+
+      <TextInput onPress={onPress} placeholder={placeholder} value="" onChangeText={()=>{}} placeholderTextColor="#a8b5db" className='flex-1 ml-2 text-white' />
     </View>
   )
 }

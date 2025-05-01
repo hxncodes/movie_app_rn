455e0f3d7e77aa26cd88ec8946cada59f59e72de
hxncodes <hxncodes@gmail.com>
2025-05-01 19:13:04 +0500

    useFecth hook created

--- services/api.ts
+++ services/api.ts
@@ -8,14 +8,19 @@ export const TMDB_CONFIG = {
 };
 
 
-export const fetchMovies = async({query}:{query:string})=>{
-    const endpoint = query ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}` : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
-    const response = await fetch(endpoint, {method: 'GET', headers: TMDB_CONFIG.headers});
-    if (!response.ok) {
-        throw new Error('Failed to fetch movies', response.statusText);
-    }
-    const data = await response.json();
-    return data.results;
+export const fetchMovies = async ({ query }: { query: string }) => {
+  const endpoint = query ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}` :
+    `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
+  const response = await fetch(endpoint, {
+    method: 'GET',
+    headers: TMDB_CONFIG.headers,
+  })
+  if (!response.ok) {
+    // @ts-ignore
+    throw new Error('Failed to fetch movies', response.statusText);
+  }
+  const data = await response.json();
+  return data.results;
 }
 const url = "keyword/{keyword_id}/movie";
 const options = {
